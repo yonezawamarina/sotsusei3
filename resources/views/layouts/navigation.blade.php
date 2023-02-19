a73444a248c51c99fe7056b3a8534fac8ce47707
yonezawamarina <so0ra7ma1me5@yahoo.co.jp>
2023-02-19 15:32:34 +0000

    ホーム画面

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,13 +12,20 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    <!--<x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">-->
+                    <!--    {{ __('Dashboard') }}-->
+                    <!--</x-nav-link>-->
                      <x-nav-link :href="route('dogs.index')" :active="request()->routeIs('dogs.index')">
-                       わんちゃん情報
+                       HOME
                         </x-nav-link>
+                        <x-nav-link :href="route('dogs.create')" :active="request()->routeIs('dogs.index')">
+                       新規登録
+                     </x-nav-link>
                 </div>
+                
+                <!-- Navigation Links -->
+                
+                
             </div>
 
             <!-- Settings Dropdown -->
