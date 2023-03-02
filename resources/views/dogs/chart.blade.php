de3d01eb6e21b351074331d6309a2443d58db3e6
yonezawamarina <so0ra7ma1me5@yahoo.co.jp>
2023-03-02 15:03:43 +0000

    レーダーチャート初期

--- resources/views/dogs/chart.blade.php
+++ resources/views/dogs/chart.blade.php
@@ -6,13 +6,6 @@
     </x-slot>
     
          
-        
-        
-         <!--その犬の１日のカロリー摂取量を満たす為の、選択したフードのグラム数を求めたい-->
-         <!--上記の数式　"$dog->calorie"　÷（割る）"選択したドッグフードのカロリdog_food->kilocalorieー"　×１００-->
-         <!---->
-    
-    
     
          
         <div>{{$dog->name}}</div> 
@@ -94,7 +87,7 @@
 
                 const ctx = document.getElementById("mychart").getContext("2d");
                 const myChart = new Chart(ctx, {
-                    type: "bar",
+                    type: "radar",
                     data: {
                         labels: ["タンパク質", "ビタミン", "脂質", "炭水化物"],
                         datasets: [
@@ -107,7 +100,7 @@
                                     "rgba(255, 206, 86, 0.2)",
                                     "rgba(75, 192, 192, 0.2)",
                                     "rgba(153, 102, 255, 0.2)",
-                                    "rgba(255, 159, 64, 0.2)",
+                                    
                                 ],
                                 borderColor: [
                                     "rgba(255, 99, 132, 1)",
@@ -115,10 +108,38 @@
                                     "rgba(255, 206, 86, 1)",
                                     "rgba(75, 192, 192, 1)",
                                     "rgba(153, 102, 255, 1)",
-                                    "rgba(255, 159, 64, 1)",
+                                    
                                 ],
                                 borderWidth: 1,
                             },
+                            
+                            {
+                                label: "# of Votes",
+                                data: [0.1,0.2,0.3,0.4],
+                                backgroundColor: [
+                                    "rgba(255, 99, 132, 0.2)",
+                                    "rgba(54, 162, 235, 0.2)",
+                                    "rgba(255, 206, 86, 0.2)",
+                                    "rgba(75, 192, 192, 0.2)",
+                                    "rgba(153, 102, 255, 0.2)",
+                                    
+                                ],
+                                borderColor: [
+                                    "rgba(255, 99, 132, 1)",
+                                    "rgba(54, 162, 235, 1)",
+                                    "rgba(255, 206, 86, 1)",
+                                    "rgba(75, 192, 192, 1)",
+                                    "rgba(153, 102, 255, 1)",
+                                    
+                                ],
+                                borderWidth: 1,
+                            },
+                            
+                            
+                            
+                            
+                            
+                            
                         ],
                     },
                     options: {
