bd58d8da5b2218b65b34689beaf8978284286743
yonezawamarina <so0ra7ma1me5@yahoo.co.jp>
2023-03-14 13:38:49 +0000

    3月14日

--- resources/views/dogs/create.blade.php
+++ resources/views/dogs/create.blade.php
@@ -76,13 +76,14 @@
                                 </div>
                                 
                                 <!--画像登録-->
+                                 <label for="age" class="leading-7 text-sm text-gray-600">わんちゃんの写真</label></br>
                                  <input type="file" name="image">
                                 
   
                            
                             
                                 <div class="p-2 w-full">
-                                  <button class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">確認</button>
+                                  <button class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">送信</button>
                                 </div>
                 
                               </div>
