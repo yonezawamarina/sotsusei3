3ec31007a2f20aed25a69eb7d3c440d67381c3e5
yonezawamarina <so0ra7ma1me5@yahoo.co.jp>
2023-03-25 06:51:36 +0000

    3月25日2回目

--- resources/views/recipes/gorecipe.blade.php
+++ resources/views/recipes/gorecipe.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            レシピ詳細{{session('dog')->name}}のごはん一覧
+            レシピ詳細
         </h2>
     </x-slot>
 
