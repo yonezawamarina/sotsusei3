1df6d9288ef2c0a9e6ea290e2674396eb1ed73b8
yonezawamarina <so0ra7ma1me5@yahoo.co.jp>
2023-03-28 15:25:46 +0000

    3月28日3

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -10,30 +10,18 @@ use Illuminate\Http\Request;
 class LineMessengerController extends Controller
 {
        public function webhook(Request $request) {
-        // LINEから送られた内容を$inputsに代入
-        $inputs=$request->all();
-        // dd(inputs);
- 
-        // そこからtypeをとりだし、$message_typeに代入
-        $message_type=$inputs['events'][0]['type'];
- 
-        // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if($message_type=='message') {
-            
-            // replyTokenを取得
-        $reply_token=$inputs['events'][0]['replyToken'];
- 
-        // LINEBOTSDKの設定
-        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
-        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+         $http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(config('services.line.access_token'));
+        $bot = new \LINE\LINEBot($http_client, ['channelSecret' => config('services.line.message_channel_secret')]);
 
-        // 送信するメッセージの設定
-        $reply_message='メッセージありがとうございます';
+        $message = "LINE送信テスト";
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+        $response = $bot->pushMessage($lineUserId, $textMessageBuilder);
 
-        // ユーザーにメッセージを返す
-        $reply=$bot->replyText($reply_token, $reply_message);
-        
-        return 'ok';
+        if ($response->isSucceeded()) {
+            Log::info('送信成功');
+        } else {
+            Log::error('送信失敗: '. $response->getHTTPStatus(). ' '. $response->getRawBody());
+        }
         }
     }
-}
+
